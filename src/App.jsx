eb379db19e55190120fa448d14c24b5561f721cf
LinCarbajales <lincarbajales@gmail.com>
2026-02-03 14:57:03 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Calculadora de ingresos en Kindle Unlimited</h1>
+      <p>Aplicación en desarrollo</p>
+    </div>
+  )
+}
+
+export default App
